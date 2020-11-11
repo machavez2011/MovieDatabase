fe9f16944375c867069d21520c1bc74568dcb092
Michael Chavez <machavez2011@gmail.com>
2020-11-11 16:14:54 -0700

    Application is now an SPA and can query for a specific movie withe results being displayed.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-alert('Movie Database');
+let moviesDatabase = (function() {
+    function render() {
+        const main = document.querySelector('#main');
+        const header = '<h1>Movie Database</h1>';
+        const input = "<input id='movieQuery' type='text' placeholder='Enter Movie here'>";
+        const search = "<button id='searchButton'>Search</button>";
+        const result = "<ul id='resultList'></ul>";
+        main.innerHTML = header + input + search + result;
+        document.querySelector('#searchButton').addEventListener('click', moviesDatabase.getMovies);
+    }
+
+    function getMovies() {
+        const movieQuery = document.querySelector('#movieQuery');
+        fetch("https://imdb8.p.rapidapi.com/title/auto-complete?q=" + movieQuery.value, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "7f66dbc693msh6d45b0305e5f07ap14b428jsn8eeed1d35807",
+                "x-rapidapi-host": "imdb8.p.rapidapi.com"
+            }
+        })
+        .then(response => response.json())
+        .then(data => populateList(data))
+        .catch(err => {
+            console.error(err);
+        });
+    }
+
+    function populateList(data) {
+        const resultList = document.querySelector('#resultList');
+        while(resultList.firstChild) {
+            resultList.removeChild(resultList.firstChild)
+        }
+
+        data.d.forEach(movie => {
+            newLi = document.createElement('li');
+            newLi.innerHTML = movie.l;
+
+            resultList.appendChild(newLi);
+        });
+
+        movieQuery.value = '';
+    }
+
+    return {
+        render,
+        getMovies
+    }
+})()
+
+moviesDatabase.render();
